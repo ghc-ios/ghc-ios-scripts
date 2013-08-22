1f2b12d8a6e04ca8230fef1607a26b29b9a49119
Luke Iannini <lukexi@me.com>
2013-08-22 15:22:00 -0700

    Added wrapper for Xcode 5's clang

---
+++ clang-xcode5-wrapper.hs
@@ -0,0 +1,35 @@
+import System.Environment
+import System.Process
+
+-- | This script wraps clang to pass it essential arguments that keep it from dying when
+-- GHC uses it as a C pre-processor during the build process.
+-- To use it, choose the version of clang you'd like to use, 
+-- (GHC-iOS needs to use Xcode5's clang, which is already set here)
+-- then compile it (ghc clang.hs).
+-- Then, find your GHC settings file (usually something like /usr/local/lib/ghc-7.6.3/settings)
+-- and change the "C compiler command" to point to the resultant wrapper executable.
+
+-- N.B. you can also just put it in your PATH, which lets you refer to it simply 
+-- as "clang" in GHC settings and "./configure --with-gcc=clang"
+
+clang = "/usr/bin/clang" -- system clang
+
+flags = 
+	[ "-Wno-invalid-pp-token" 
+	, "-Wno-unicode"
+	, "-Wno-trigraphs"
+	]
+
+-- See if we're in preprocessor mode
+check args@("-E":"-undef":"-traditional":_) = replace args
+check other = other
+
+-- make sure we use assembler-with-cpp
+replace ("-x":"c":xs) = "-x":"assembler-with-cpp":replace xs
+replace (x:xs) = x:replace xs
+replace [] = []
+
+main = do
+	args <- getArgs
+	rawSystem clang $ flags ++ (check args)
+
