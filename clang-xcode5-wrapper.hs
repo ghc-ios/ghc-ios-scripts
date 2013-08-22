0baf41159e9520aedcf3f27a8aada262efbf4780
Luke Iannini <lukexi@me.com>
2013-08-22 15:30:12 -0700

    Doc fix

--- clang-xcode5-wrapper.hs
+++ clang-xcode5-wrapper.hs
@@ -5,12 +5,12 @@ import System.Process
 -- GHC uses it as a C pre-processor during the build process.
 -- To use it, choose the version of clang you'd like to use, 
 -- (GHC-iOS needs to use Xcode5's clang, which is already set here)
--- then compile it (ghc clang.hs).
+-- then compile it (ghc clang-xcode5-wrapper.hs).
 -- Then, find your GHC settings file (usually something like /usr/local/lib/ghc-7.6.3/settings)
 -- and change the "C compiler command" to point to the resultant wrapper executable.
 
 -- N.B. you can also just put it in your PATH, which lets you refer to it simply 
--- as "clang" in GHC settings and "./configure --with-gcc=clang"
+-- as "clang-xcode5-wrapper" in GHC settings and "./configure --with-gcc=clang-xcode5-wrapper"
 
 clang = "/usr/bin/clang" -- system clang
 
