811538eafdd4c826a5689ab2720fc38959dbee12
Luke Iannini <lukexi@me.com>
2014-05-22 14:43:06 -0700

    Add a fix for a screwy 7.8.2 arm bindist. Remove tars after unpack

--- installGHCiOS.sh
+++ installGHCiOS.sh
@@ -4,11 +4,14 @@ echo "Downloading GHC for iOS devices..."
 cd /tmp
 wget https://www.haskell.org/ghc/dist/7.8.2/ghc-7.8.2-arm-apple-ios.tar.xz
 tar xvf ghc-7.8.2-arm-apple-ios.tar.xz && mv ghc-7.8.2 ghc-7.8.2-arm
+rm ghc-7.8.2-arm-apple-ios.tar.xz
 cd ghc-7.8.2-arm
 ./configure
 # Fix the settings file to point to the right scripts
 sed -i '' 's|/usr/bin/gcc|arm-apple-darwin10-clang|g' settings
 sed -i '' 's|/usr/bin/ld|arm-apple-darwin10-ld|g' settings
+# Fix a screwy 7.8.2 arm bindist. Can be removed for 7.8.3.
+find ./ -type f -exec sed -i -e 's/cfgOnAppThread//g' {} \;
 sudo make install
 
 
@@ -16,6 +19,7 @@ echo "Downloading GHC for the iOS simulator..."
 cd /tmp
 wget https://www.haskell.org/ghc/dist/7.8.2/ghc-7.8.2-i386-apple-ios.tar.xz
 tar xvf ghc-7.8.2-i386-apple-ios.tar.xz && mv ghc-7.8.2 ghc-7.8.2-i386
+rm ghc-7.8.2-i386-apple-ios.tar.xz
 cd ghc-7.8.2-i386
 ./configure
 # Fix the settings file to point to the right scripts
